5ac02d465805796d337c937cb1e261e74c424a5a
TinyKitten <catwalk8205@gmail.com>
2017-12-08 00:32:06 +0900

    DI設定構成ミス修正

--- src/environments/environment.staging.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiEndpoint: 'https://kittentlapi-stag.herokuapp.com/1.0',
-  wsEndpoint: 'wss://kittentlapi-stag.herokuapp.com/1.0',
-  localStorageToken: 'secret'
-};
