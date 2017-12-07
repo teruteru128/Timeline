606ee3054ac861ac6e8610a701ba98ee3bb1231c
TinyKitten <catwalk8205@gmail.com>
2017-12-08 00:07:24 +0900

    staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiEndpoint: 'https://kittentlapi-stag.herokuapp.com/1.0',
+  wsEndpoint: 'wss://kittentlapi-stag.herokuapp.com/1.0',
+  localStorageToken: 'secret'
+};
