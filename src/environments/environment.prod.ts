40815ae49ee213959177d23ca3b30ae657058564
TinyKitten <catwalk8205@gmail.com>
2017-12-08 10:05:21 +0900

    APIエンドポイント変更

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://kittentlapi.herokuapp.com/1.0',
-  wsEndpoint: 'wss://kittentlapi.herokuapp.com/1.0',
-  localStorageToken: 'secret'
+  apiEndpoint: 'https://tlapi.tinykitten.me/1.0',
+  wsEndpoint: 'wss://tlapi.tinykitten.me/1.0',
+  localStorageToken: 'g1vhjwiM1ddzFLO5U3tDUd7xB292rxlc'
 };
