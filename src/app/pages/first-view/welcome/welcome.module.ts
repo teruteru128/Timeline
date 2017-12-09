b1dde8e02e57802857cedde551d2c71245c59259
TinyKitten <catwalk8205@gmail.com>
2017-12-09 09:28:14 +0900

    ティザーサイトをログイン前ページに採用

--- src/app/pages/first-view/welcome/welcome.module.ts
+++ src/app/pages/first-view/welcome/welcome.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
